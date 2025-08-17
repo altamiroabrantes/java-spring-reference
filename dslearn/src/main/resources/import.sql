97fddc983880246cd1a5348dc035806122d2f457
Altamiro Abrantes <altamiropina.psti@poupex.com.br>
2025-08-17 10:57:54 -0300

    Criação da Classe Course

--- dslearn/src/main/resources/import.sql
+++ dslearn/src/main/resources/import.sql
@@ -12,3 +12,5 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+
+INSERT INTO tb_course (name, img_uri, img_gray_uri) VALUES ('Bootcamp HML','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHc8Qios7XTdFctHqLYLYKAkR6kDS9gqHA0g&s','https://blogcorretor.aocubo.com/wp-content/uploads/2022/01/aaasd.jpg');
