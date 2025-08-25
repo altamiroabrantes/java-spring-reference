1ffcf508f991645e4ca8195b5b01472c2dfa68e5
altam <altam@DESKTOP-4N5PT2M>
2025-08-25 11:53:28 -0300

    Lesson, Content, Task

---
+++ dslearn/src/main/java/com/devsuperior/dslearn/entities/Content.java
@@ -0,0 +1,30 @@
+package com.devsuperior.dslearn.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_content")
+public class Content extends Lesson{
+	
+	private static final long serialVersionUID = 1L;
+
+	private String textContent;
+	private String videoUri;
+	
+	public Content() {}
+
+	public Content(Long id, String title, Integer position, Section section, String textContent, String videoUri) {
+		super(id, title, position, section);
+		this.textContent = textContent;
+		this.videoUri = videoUri;
+	}
+
+	
+	
+	
+}
---
+++ dslearn/src/main/java/com/devsuperior/dslearn/entities/Task.java
@@ -0,0 +1,39 @@
+package com.devsuperior.dslearn.entities;
+
+import java.time.Instant;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_task")
+public class Task extends Lesson{
+
+	private static final long serialVersionUID = 1L;
+	
+	private String descriptiom;
+	private Integer questionCount;
+	private Integer approvalCount;
+	private Double weight;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant dueDate;
+
+	public Task() {}
+
+	public Task(Long id, String title, Integer position, Section section, String descriptiom, Integer questionCount,
+			Integer approvalCount, Double weight, Instant dueDate) {
+		super(id, title, position, section);
+		this.descriptiom = descriptiom;
+		this.questionCount = questionCount;
+		this.approvalCount = approvalCount;
+		this.weight = weight;
+		this.dueDate = dueDate;
+	}
+
+}
