db01cc5e8e924586482c50e5e843b52f3053f82b
altam <altam@DESKTOP-4N5PT2M>
2025-09-05 10:22:15 -0300

    Lesson seed, fix: lessonsDone

--- dslearn/src/main/java/com/devsuperior/dslearn/entities/Enrollment.java
+++ dslearn/src/main/java/com/devsuperior/dslearn/entities/Enrollment.java
@@ -1,14 +1,18 @@
 package com.devsuperior.dslearn.entities;
 
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 import com.devsuperior.dslearn.entities.pk.EnrollmentPK;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
+import lombok.Getter;
 
 @Data
 @Entity
@@ -26,6 +30,10 @@ public class Enrollment {
 	
 	private boolean available;
 	private boolean onlyUpdate;
+	
+	@Getter
+	@ManyToMany(mappedBy = "enrollmentsDone")
+	private Set<Lesson> lessonsDone = new HashSet<>(); 
 	
 	public Enrollment() {}
 	
--- dslearn/src/main/java/com/devsuperior/dslearn/entities/Task.java
+++ dslearn/src/main/java/com/devsuperior/dslearn/entities/Task.java
@@ -16,7 +16,7 @@ public class Task extends Lesson{
 
 	private static final long serialVersionUID = 1L;
 	
-	private String descriptiom;
+	private String description;
 	private Integer questionCount;
 	private Integer approvalCount;
 	private Double weight;
@@ -26,10 +26,10 @@ public class Task extends Lesson{
 
 	public Task() {}
 
-	public Task(Long id, String title, Integer position, Section section, String descriptiom, Integer questionCount,
+	public Task(Long id, String title, Integer position, Section section, String description, Integer questionCount,
 			Integer approvalCount, Double weight, Instant dueDate) {
 		super(id, title, position, section);
-		this.descriptiom = descriptiom;
+		this.description = description;
 		this.questionCount = questionCount;
 		this.approvalCount = approvalCount;
 		this.weight = weight;
